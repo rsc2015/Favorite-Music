94d06a697bcdb6f33f117cca3bec570946639ea8
ROHINI CHAVAN <rohinichavan@ROHINIs-MacBook-Pro.local>
2018-02-17 17:09:58 -0600

    initial code

---
+++ arraymethods.js
@@ -0,0 +1,7 @@
+console.log("arraymethods.js");
+
+let artist = ["Charli", "Halsey", "Harry Hudson", "The Weeknd", "Bastille", "Justin Timberlake", "twenty one pilots", " Taylor Swift", "Hozier", "Drake"];
+
+let songs = ["Out 0f my head", "Bad at Love", "Gone", "Secrets", "Glory", "Supplies", "Stressed Out", "Look What You Made Me Do", "Take Me To Church", "Diplomatic Immunity"];
+
+let album = ["Pop 2", "hopeless fountain kingdom", "Hudson", "STARBOY", "Wild world", "Man of the Woods", "Blurryface", "reputation", "Hozier album", "Scary Hours"];
